bede7429799f9690741d6bfecfbb188c1e959e8c
MarcelMatejak-Niemczuk <klocekkube@gmail.com>
2025-06-11 08:26:43 +0200

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,24 +1,24 @@
 function zmientlo() {
-    const wyraz = document.getElementById(napis)
-    napis.classList.replace("rozmiar","tlo")
+    const wyraz = document.getElementById("napis");
+    wyraz.classList.replace("rozmiar", "tlo");
 }
 
 function zmienrozmiar() {
-    const wyraz = document.getElementById(napis)
-    napis.classList.add("rozmiar")
+    const wyraz = document.getElementById("napis");
+    wyraz.classList.add("rozmiar");
 }
 
 function zmienkroj() {
-    const wyraz = document.getElementById(napis)
-    napis.classList.add("kroj")
+    const wyraz = document.getElementById("napis");
+    wyraz.classList.add("kroj");
 }
 
 function usunEfekty() {
-    const wyraz = document.getElementById(napis)
-    napis.classList.remove("tlo", "rozmiar", "kroj")
+    const wyraz = document.getElementById("napis");
+    wyraz.classList.remove("tlo", "rozmiar", "kroj", "skresl");
 }
 
 function skresltekst() {
-    const wyraz = document.getElementById(napis).classlist.add("skresl")
-    
+    const wyraz = document.getElementById("napis");
+    wyraz.classList.add("skresl");
 }
